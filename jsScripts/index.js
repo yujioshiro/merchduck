67101088b052fd69bcffa18ffd7a464bd6138399
Yuji Oshiro <Iamyujioshiro@gmail.com>
2023-03-24 17:45:56 -0700

    Landing page redesign (#6)
    * removed files
    
    * updated gitignore
    
    * update htaccess
    
    * update favicon
    
    * add site manifest
    
    * add browserconfig
    
    * redesigned homepage
    
    * d

---
+++ jsScripts/index.js
@@ -0,0 +1,64 @@
+let adjustTopMargin = () => {
+    let firstContent = document.getElementsByClassName("first-content-after-navbar")[0];
+    let navBar = document.getElementsByTagName("nav")[0];
+    firstContent.style.marginTop = navBar.offsetHeight + "px";
+  }
+  
+adjustTopMargin();
+
+(() => {
+    let footer = document.getElementsByTagName("footer")[0];
+    let navBar = document.getElementsByTagName("nav")[0];
+    let gallerySection = document.getElementById("more-work-gallery");
+    
+    footer.style.height = navBar.offsetHeight;
+    gallerySection.style.height = window.innerHeight - footer.offsetHeight - footer.offsetHeight + "px";
+
+})()
+
+// var mouse = {'x': 0, 'y': 0};
+
+// homex = 0;
+// homey = 0;
+// forcex = 0;
+// forcey = 0;
+// magnet = 10;
+
+// let objectsToJiggle = document.getElementsByClassName("jiggle");
+
+// for (let i = 0; i < 2; i++) { 
+//     objectsToJiggle[i].setAttribute('homex', objectsToJiggle[i].getBoundingClientRect().left);
+//     objectsToJiggle[i].setAttribute('homey', objectsToJiggle[i].getBoundingClientRect().top);
+//     console.log("homex: " + objectsToJiggle[i].getAttribute('homex'));
+//     console.log("homey: " + objectsToJiggle[i].getAttribute('homey'));
+
+// }
+
+// document.addEventListener('mousemove', function(e) {
+//     mouse = {'x': e.pageX, 'y': e.pageY};
+// });
+
+// setInterval(() => {
+//     for (let i = 0; i < 2; i++) {
+//         position = objectsToJiggle[i].position;
+//         x0 = objectsToJiggle[i].offsetLeft;
+//         y0 = objectsToJiggle[i].offsetTop;
+//         x1 = mouse.x - (objectsToJiggle[i].style.width / 2);
+//         y1 = mouse.y - (objectsToJiggle[i].style.width / 2);
+//         distancex = x1 - x0;
+//         distancey = y1 - y0;
+
+//         distance = Math.sqrt((distancex * distancex) + (distancey * distancey));
+//         magnet = distance / 1000; // this changes the strength of the magnet relative to distance
+                    
+//         powerx = x0 - (distancex / distance) * magnet / distance;
+//         powery = y0 - (distancey / distance) * magnet / distance;
+        
+//         forcex = (forcex + (objectsToJiggle[i].getAttribute('homex') - x0) / 2) / 2;
+//         forcey = (forcey + (objectsToJiggle[i].getAttribute('homey') - y0) / 2) / 2;
+
+//         objectsToJiggle[i].style.left = powerx + forcex + "px";
+//         objectsToJiggle[i].style.top = powery + forcey + "px";
+//     }
+//     console.log(objectsToJiggle[0].getAttribute('homex'));
+// }, 3000);
--- jsScripts/textFadeAtTop.js
+++ jsScripts/textFadeAtTop.js
@@ -1,78 +1,19 @@
-// const sectionHeadingText = document.getElementsByClassName("section-heading-text");
-// const descriptionText = document.getElementsByClassName("description-text");
 
-// for (let i = 0; i < sectionHeadingText.length; i++) {
-
-//   words = sectionHeadingText [i].innerText.split(' ');
-//   sectionHeadingText[i].innerText = "";
- 
-//   for (let j = 0; j < words.length; j++) {
-
-//     console.log(words[j]);
-//     let preTag = document.createElement('pre');
-//     preTag.innerText = words[j] + " ";
-//     preTag.classList.add('text-to-fade');
-//     sectionHeadingText[i].appendChild(preTag);
-//   }
-// }
-  
-//   for (let i = 0; i < descriptionText.length; i++) {
-
-//     words = descriptionText[i].innerText.split(' ');
-//     descriptionText[i].innerText = "";
-   
-//     for (let j = 0; j < words.length; j++) {
-  
-//       console.log(words[j]);
-//       let preTag = document.createElement('pre');
-//       preTag.innerText = words[j] + " ";
-//       preTag.classList.add('text-to-fade');
-//     descriptionText[i].appendChild(preTag);
-//     }
-//   }
-  
-
-
-// const tagline = document.getElementsByClassName("tagline")[0];
-const navbar = document.getElementsByTagName("NAVBAR")[0];
-const textToFade = document.getElementsByClassName("text-to-fade");
-
-window.onscroll = function() {
-    console.log(" Y-axis : " + pageYOffset) //shows Y Offset in console
-  
-    // Fading text as it reaches top of viewportHeight
-  
-    for (let i = 0; i < textToFade.length; i++) {
-      if (textToFade[i].getBoundingClientRect().top < (navbar.scrollHeight / 2)) {
-        textToFade[i].style.opacity = "0";
-      } else if (textToFade[i].getBoundingClientRect().top > (navbar.scrollHeight + 10)) {
-        textToFade[i].style.opacity = "1";
-      } else {
-        textToFade[i].style.opacity = (textToFade[i].getBoundingClientRect().top - (navbar.scrollHeight / 2)) / ((navbar.scrollHeight + 10) - (navbar.scrollHeight / 2));
-      }
-    }
-  }
-
-
-// // const tagline = document.getElementsByClassName("tagline")[0];
 // const navbar = document.getElementsByTagName("NAVBAR")[0];
-// const wordsOnMainPage = document.getElementsByClassName("word-on-main-page");
-// const body = document.getElementsByTagName("BODY")[0];
-// const contentContainer = document.getElementsByClassName("information-section")[0];
-// const everythingElseContainer = document.getElementsByClassName("everything-else-container")[0];
+// const textToFade = document.getElementsByClassName("text-to-fade");
 
-// document.onscroll = function() {
-//     console.log(" Y-axis : " + pageYOffset) //shows Y Offset in console
+// window.onscroll = function() {
+//     // console.log(" Y-axis : " + pageYOffset) //shows Y Offset in console
   
 //     // Fading text as it reaches top of viewportHeight
   
-//     for (let i = 0; i < wordsOnMainPage.length; i++) {
-//       if (wordsOnMainPage[i].getBoundingClientRect().top < (navbar.scrollHeight / 2)) {
-//         wordsOnMainPage[i].style.opacity = "0";
-//       } else if (wordsOnMainPage[i].getBoundingClientRect().top > (navbar.scrollHeight + 10)) {
-//         wordsOnMainPage[i].style.opacity = "1";
+//     for (let i = 0; i < textToFade.length; i++) {
+//       if (textToFade[i].getBoundingClientRect().top < (navbar.scrollHeight / 2)) {
+//         textToFade[i].style.opacity = "0";
+//       } else if (textToFade[i].getBoundingClientRect().top > (navbar.scrollHeight + 10)) {
+//         textToFade[i].style.opacity = "1";
 //       } else {
-//         wordsOnMainPage[i].style.opacity = (wordsOnMainPage[i].getBoundingClientRect().top - (navbar.scrollHeight / 2)) / ((navbar.scrollHeight + 10) - (navbar.scrollHeight / 2));
+//         textToFade[i].style.opacity = (textToFade[i].getBoundingClientRect().top - (navbar.scrollHeight / 2)) / ((navbar.scrollHeight + 10) - (navbar.scrollHeight / 2));
 //       }
 //     }
 //   }
